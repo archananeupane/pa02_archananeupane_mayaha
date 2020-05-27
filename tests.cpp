b98847d874e925472694da2e89d2da6bf015448a
Archana Neupane <archananeupane@ucsb.edu>
2020-05-26 17:23:53 -0700

    update

--- tests.cpp
+++ tests.cpp
@@ -6,6 +6,39 @@ using namespace std;
 #include <vector>
 
 int main(){
+runAll();
+return 0;
+}
+
+void runAll(){
+    test_insert();
+    test_member_functions();
+    test_accessors_mutators();
+}
+
+void test_insert(){
+    START_TEST("test_insert");
+    test_insert_empty_list();
+    //test_insert_single_node();
+    END_TEST("test_insert");
+}
+
+void test_member_functions(){
+        START_TEST("test_member_functions");
+        //functions
+        END_TEST("test_member_functions");
+}
+
+void test_accessors_mutators(){
+    START_TEST("test_accessors_mutators");
+    //functions
+    END_TEST("test_accessors_mutators");
+}
 
 
+void test_insert_empty_list(){
+    string testname = "append node Lion King with rating 8.5";
+    BST b1;
+    b1.insert("Lion King", 8.5);
+    assertEquals("Lion King", b1.getRoot()->getMovieName(), testname);
 }
--- tests.h
+++ tests.h
@@ -4,8 +4,30 @@
 #include <iostream>
 using namespace std; 
 
+void runAll();
+void test_insert();
+void test_member_functions();
+void test_accessors_mutators();
+
+void test_insert_empty_list();
+//void test_insert_single_node();
 
 
+void START_TEST(string testname){
+  cout<<"Start "<<testname<<endl;
+}
+
+void END_TEST(string testname) {
+  cout<<"End "<<testname<<endl<<endl;
+}
+
+void assertEquals(string expected, string actual, string testDescription){
+  if (expected == actual) {
+    cout<<"PASSED " << endl;
+  } else {
+    cout<< "  FAILED: "<< testDescription << endl <<"   Expected: "<< expected << " Actual: " << actual << endl;
+  }
+}
 
 
 
